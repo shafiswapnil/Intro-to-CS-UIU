87a37080478d2c86be679b4d1aed508d10f31fa1
Shafi Swapnil <swapnil.env@gmail.com>
2022-11-16 09:48:03 +0600

    solve problem set 2

--- problem-set-2/main.c
+++ problem-set-2/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    // SET A
+    float diameter, thickness;
+
+    printf("Please mention the diameter (between 6 inch and 18 inch)\n");
+    scanf("%f", &diameter);
+    printf("Please mention the thickness (from 0.5 inch to 2 inch)\n");
+    scanf("%f", &thickness);
+
+    // calculation
+    float radius = diameter/2;
+    float volume = M_PI*(radius*radius)*thickness;
+
+    printf("Volume = %f\n", volume);
+
+    float price_per_diameter = 25;
+    float price = volume*price_per_diameter;
+    printf("Price = %f", price);
+
     return 0;
 }
